cc474b09ab259840bb92635809d14874ac6ecdbb
dmaher42 <Daniel.maher601@schools.sa.edu.au>
2025-09-12 21:29:48 +0930

    Build Tailwind CSS for production and link app.css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html",
+    "./**/*.html",
+    "./**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
