9d81b4446d2f08f8aed1f998fb7a24dfb9d99eb5
dmaher42 <Daniel.maher601@schools.sa.edu.au>
2025-10-12 13:48:35 +1030

    refactor: move supabase env to separate module

---
+++ js/env.js
@@ -0,0 +1,6 @@
+/* BEGIN GPT CHANGE: env module */
+export const ENV = {
+  SUPABASE_URL: window?.__ENV?.SUPABASE_URL ?? "",
+  SUPABASE_ANON_KEY: window?.__ENV?.SUPABASE_ANON_KEY ?? "",
+};
+/* END GPT CHANGE */
