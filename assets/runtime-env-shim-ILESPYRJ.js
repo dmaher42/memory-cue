786b9fe7779cfa1fb20d72dfe4f12e2c276c0078
dmaher42 <dmaher42@users.noreply.github.com>
2025-12-27 10:21:37 +0000

    deploy: 260990f7b868c2e4dea197cac549041af66389b8

---
+++ assets/runtime-env-shim-ILESPYRJ.js
@@ -0,0 +1,1 @@
+(()=>{var w=(r,t)=>()=>(t||r((t={exports:{}}).exports,t),t.exports);var n=w(()=>{window.__ENV=window.__ENV||{};window.textureUrl=window.textureUrl||(r=>typeof r!="string"?"":r)});n();})();
--- assets/runtime-env-shim-TYQCA2C5.js
+++
@@ -1,1 +0,0 @@
-(()=>{var d=(_,w)=>()=>(w||_((w={exports:{}}).exports,w),w.exports);var i=d(()=>{window.__ENV=window.__ENV||{}});i();})();
