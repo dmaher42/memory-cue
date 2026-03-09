de96ce32daa8f2ac17169de3084354618b5ce43a
dmaher42 <dmaher42@users.noreply.github.com>
2026-03-09 11:21:29 +0000

    deploy: d5d8b1c0dcf960bdbe4e16233bf9b2f888d318b4

--- assets/runtime-env-shim-ILESPYRJ.js
+++
@@ -1,1 +0,0 @@
-(()=>{var w=(r,t)=>()=>(t||r((t={exports:{}}).exports,t),t.exports);var n=w(()=>{window.__ENV=window.__ENV||{};window.textureUrl=window.textureUrl||(r=>typeof r!="string"?"":r)});n();})();
--- assets/update-footer-year-EFMYO4DK.js
+++
@@ -1,1 +0,0 @@
-(()=>{var a=(n,e)=>()=>(e||n((e={exports:{}}).exports,e),e.exports);var l=a(()=>{var t=document.getElementById("year");t&&(t.textContent=new Date().getFullYear())});l();})();
