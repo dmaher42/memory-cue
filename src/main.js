2c613406ad6addbd2ddd16bb5409c8e86b78e6cd
dmaher42 <Daniel.maher601@schools.sa.edu.au>
2026-03-17 19:55:52 +1030

    Switch production build to Vite entry bundling

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import '../js/init-env.js';
+import '../js/config-supabase.js';
+import '../js/services/capture-service.js';
+import '../js/services/assistant-service.js';
+import '../js/entries.js';
+import '../js/reminders.js';
+import '../mobile.js';
+import '../js/mobile-theme-toggle.js';
