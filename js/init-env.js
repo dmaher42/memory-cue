b9016a91348fb42a4f7539a26dc444c05fd4e804
dmaher42 <Daniel.maher601@schools.sa.edu.au>
2026-03-18 20:25:38 +1030

    Stabilize runtime env bootstrap

--- js/init-env.js
+++ js/init-env.js
@@ -1,5 +1,20 @@
+const DEFAULT_ENV = {
+  GOOGLE_SCRIPT_ENDPOINT:
+    'https://script.google.com/macros/s/AKfycbylH5GmqeojNoZ-MA9WRg-w1S-ei9cv8Jo1M0qL7t5cn59LBRCCJ779WOyLi7qQwkSx/exec',
+};
+
 if (typeof window !== 'undefined') {
-  const env = window.__ENV || {};
+  const existingEnv =
+    window.__ENV && typeof window.__ENV === 'object' && !Array.isArray(window.__ENV)
+      ? window.__ENV
+      : {};
+
+  window.__ENV = {
+    ...DEFAULT_ENV,
+    ...existingEnv,
+  };
+
+  const env = window.__ENV;
   const hasFirebaseConfig = Boolean(
     env.FIREBASE_API_KEY && env.FIREBASE_AUTH_DOMAIN && env.FIREBASE_PROJECT_ID && env.FIREBASE_APP_ID
   );
--- js/storage.js
+++ js/storage.js
@@ -1,8 +1,3 @@
-window.__ENV = {
-  ...window.__ENV,
-  GOOGLE_SCRIPT_ENDPOINT: "https://script.google.com/macros/s/AKfycbylH5GmqeojNoZ-MA9WRg-w1S-ei9cv8Jo1M0qL7t5cn59LBRCCJ779WOyLi7qQwkSx/exec"
-};
-
 (function() {
   // Enhanced Notes Editor
   const notesEditor = document.getElementById('notes');
